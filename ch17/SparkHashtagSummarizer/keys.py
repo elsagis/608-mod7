50eb996a9bef1f86aa6cdee581e7db10add69fc1
elsagis <elsagis@yahoo.com>
2022-06-17 20:33:36 -0500

    commit

--- ch17/SparkHashtagSummarizer/keys.py
+++
@@ -1,6 +0,0 @@
-consumer_key = 'YourConsumerKey'
-consumer_secret = 'YourConsumerSecret'
-access_token = 'YourAccessToken'
-access_token_secret = 'YourAccessTokenSecret'
-
-mapquest_key = 'YourAPIKey'
--- ch17/TwitterMongoDB/keys.py
+++
@@ -1,8 +0,0 @@
-consumer_key = 'YourConsumerKey'
-consumer_secret = 'YourConsumerSecret'
-access_token = 'YourAccessToken'
-access_token_secret = 'YourAccessTokenSecret'
-
-mapquest_key = 'YourAPIKey'
-
-mongo_connection_string = 'YourMongoConnectionString'
